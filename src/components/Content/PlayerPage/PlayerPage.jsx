2451518684fd01fc0f7f80a6fddbd228f43007e8
Dazeroth <dazeroth.feral@gmail.com>
2023-05-29 19:35:13 +0300

    add test page "player page"

---
+++ src/components/Content/PlayerPage/PlayerPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import css from "./PlayerPage.module.css";
+
+const PlayerPage = () => {
+    return(
+        <div className={css.content}>
+            Hello!
+        </div>
+    )
+};
+
+export default PlayerPage;
--- src/components/Routes/RoutesBlock.jsx
+++ src/components/Routes/RoutesBlock.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 
 import MainPage from '../Content/MainPage/MainPage';
+import PlayerPage from "../Content/PlayerPage/PlayerPage";
 
 const RoutesBlock = ({state}) => {
     return(
         <Routes>
             <Route path="/" element={<MainPage state={state} />} />
+            <Route path="/player_page" element={<PlayerPage state={state} />} />
         </Routes>
     );
 };
