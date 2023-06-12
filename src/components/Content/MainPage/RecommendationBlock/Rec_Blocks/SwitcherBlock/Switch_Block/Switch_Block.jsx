b7e1f9959a5d21c0253ca3eec6fc603382490b1e
Dazeroth <dazeroth.feral@gmail.com>
2023-06-12 16:29:22 +0300

    інтерактивчик лайків в трендах

--- src/components/Content/MainPage/RecommendationBlock/Rec_Blocks/SwitcherBlock/Switch_Block/Switch_Block.jsx
+++ src/components/Content/MainPage/RecommendationBlock/Rec_Blocks/SwitcherBlock/Switch_Block/Switch_Block.jsx
@@ -3,10 +3,15 @@ import React from "react";
 import css from "./Switch_Block.module.css";
 
 const Switch_Block = ({props, set_recommendation_block, block_number}) => {
+    const handle_click = (event) => {
+        event.stopPropagation();
+        set_recommendation_block(block_number)
+    };
+    
     return(
         <div className={css.content}>
             <img className={css.switch_img} src={props.mini_img_path} />
-            <div onClick={() => set_recommendation_block(block_number)} className={css.img_overlay}>
+            <div onClick={handle_click} className={css.img_overlay}>
                 <span className={css.card_text}>
                     {props.name}
                 </span>
